f31a7bb8d25036aebfc834a4548042e96942d6dc
김용권 <yongkwon.kim@ikoob.com>
2026-02-04 23:23:24 +0900

    fix : 순환참조 오류 수정

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/UserRepositoryImpl.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/UserRepositoryImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class UserJpaRepositoryImpl implements UserRepository {
+public class UserRepositoryImpl implements UserRepository {
 
     private final UserJpaRepository userJpaRepository;
 
