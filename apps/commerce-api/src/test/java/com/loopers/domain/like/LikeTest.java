827fe4ce719be619e3db48500046765e96fb8d02
김용권 <yongkwon.kim@ikoob.com>
2026-02-23 15:55:47 +0900

    test : (좋아요 도메인) null 파라미터 추가

--- apps/commerce-api/src/test/java/com/loopers/domain/like/LikeTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/like/LikeTest.java
@@ -4,15 +4,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.loopers.support.error.CoreException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LikeTest {
 
     @DisplayName("좋아요의 상품 정보가 유효하지 않으면, 예외를 던진다")
-    @Test
-    void fail_when_invalid_ref_product_id() {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(longs = {-100L})
+    void fail_when_invalid_ref_product_id(Long refProductId) {
         Long id = 100L;
-        Long refProductId = -100L;
         Long refUserId = 100L;
 
         assertThatThrownBy(() -> Like.create(id, refProductId, refUserId))
@@ -21,11 +24,12 @@ public class LikeTest {
     }
 
     @DisplayName("좋아요의 유저 정보가 유효하지 않으면, 예외를 던진다")
-    @Test
-    void fail_when_invalid_ref_user_id() {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(longs = {-100L})
+    void fail_when_invalid_ref_user_id(Long refUserId) {
         Long id = 100L;
         Long refProductId = 100L;
-        Long refUserId = -100L;
 
         assertThatThrownBy(() -> Like.create(id, refProductId, refUserId))
             .isInstanceOf(CoreException.class)
